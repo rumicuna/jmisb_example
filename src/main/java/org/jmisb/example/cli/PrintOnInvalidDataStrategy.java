cb37bcb30b7b36ed86ffc699f1eb9247ce21051c
Brad Hards <bradh@frogmouth.net>
2020-08-13 10:43:34 +1000

    initial commit

---
+++ src/main/java/org/jmisb/example/cli/PrintOnInvalidDataStrategy.java
@@ -0,0 +1,16 @@
+package org.jmisb.example.cli;
+
+import org.jmisb.api.common.IInvalidDataHandlerStrategy;
+import org.jmisb.api.common.KlvParseException;
+import org.slf4j.Logger;
+
+/**
+ * Invalid data strategy implementation that just dumps to System.out.
+ */
+public class PrintOnInvalidDataStrategy implements IInvalidDataHandlerStrategy {
+
+    @Override
+    public void process(Logger logger, String message) throws KlvParseException {
+        System.out.println(message);
+    }
+}
